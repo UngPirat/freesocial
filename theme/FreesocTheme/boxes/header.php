6c9293e3e7a54bb9e29dd04226a855a581a60c01
Mikael Nordfeldth <mmn@hethane.se>
2012-09-04 13:52:50 +0200

    forgot to end p element

--- theme/FreesocTheme/boxes/header.php
+++ theme/FreesocTheme/boxes/header.php
@@ -1,34 +1,34 @@
 <!DOCTYPE html>
 <?php
-	$this->out->elementStart('html', array('lang'=>$this->get_lang()));
-	$this->out->elementStart('head');
-	$this->title = $this->get_title();
-	    if (isset($args['page']) && $args['page'] >= 2) {
-	        $this->title .= ' - ' . sprintf( _m('Page %s'), $args['page']);
-	    }
+    $this->out->elementStart('html', array('lang'=>$this->get_lang()));
+    $this->out->elementStart('head');
+    $this->title = $this->get_title();
+        if (isset($args['page']) && $args['page'] >= 2) {
+            $this->title .= ' - ' . sprintf( _m('Page %s'), $args['page']);
+        }
     $this->title .= ' | ';
-	$this->title .= $this->get_siteinfo('name');
-	$this->out->element('title', null, $this->title);
-	$this->head();
+    $this->title .= $this->get_siteinfo('name');
+    $this->out->element('title', null, $this->title);
+    $this->head();
 $this->out->elementEnd('head');
 $this->out->elementStart('body');
 $this->out->elementStart('div', array('id'=>'wrapper'));
 
-	$this->out->elementStart('header');
-	$this->box('site-title');
-	$this->out->elementStart('div', array('id'=>'login'));
+    $this->out->elementStart('header');
+    $this->box('site-title');
+    $this->out->elementStart('div', array('id'=>'login'));
 
     try {
-        $this->widget('Vcard', array('profile'=>$this->profile,'avatarSize'=>48));
+        $this->widget('Vcard', array('profile'=>$this->profile,'avatarSize'=>AVATAR_STREAM_SIZE));
     } catch (Exception $e) {
-		$this->out->element('p', null, _m('You are not logged in!'));
-		$this->out->elementStart('p');
-		$this->out->element('a', array('href'=>common_local_url('login')), _m('Log in'));
-		$this->out->text(_m(' or '));
-		$this->out->element('a', array('href'=>common_local_url('register')), _m('register an account'));
-		$this->out->text('.');
+        $this->out->element('p', null, _m('You are not logged in!'));
+        $this->out->elementStart('p');
+        $this->out->element('a', array('href'=>common_local_url('login')), _m('Log in'));
+        $this->out->text(_m(' or '));
+        $this->out->element('a', array('href'=>common_local_url('register')), _m('register an account'));
+        $this->out->text('.');
+        $this->out->elementEnd('p');
     }
-	$this->out->elementEnd('div');
-	$this->box('topmenu');
+    $this->out->elementEnd('div');
+    $this->box('topmenu');
 $this->out->elementEnd('header');
-$this->out->flush();
